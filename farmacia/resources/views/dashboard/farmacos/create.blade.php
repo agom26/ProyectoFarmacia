5529aedc537e7a679f2ac3edfdfcc1fec98be0e3
and135cd <and135cd@mesoamericana.edu.gt>
2022-09-21 22:09:56 -0600

    show, edit, borrar, formfarmaco, controlador,

---
+++ farmacia/resources/views/dashboard/farmacos/create.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+    <title>Farmacos</title>
+</head>
+<body>
+   
+
+    <header>
+        @include('dashboard.partials.nav-header-admin')
+        <h5>ADMIN</h5>
+        <center><h1 class="mb-5">Ingreso de Farmacos</h1></center>
+    </header>
+
+    <main>
+        <div class="container">
+
+
+            <form action="{{route('farmacos.store')}}" method="post"> 
+                @include('dashboard.partials.sesion-flash-status')
+        
+                <section class="row 6">
+                    <article class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+                    </article>
+                    <article class="col-xs-12 col-sm-12 col-md-6 col-lg-6"> <!-- si -->
+                        <div class="form-floating mb-3">
+                            <input type="text" name="codigo" class="form-control form-floating mb-3" placeholder="Codigo" >
+                            <label for="text">Codigo</label>
+                            @error('codigo')
+                                <small class="text-danger ">{{ $message }}</small>
+                            @enderror
+                        </div>
+        
+                        <div class="form-floating mb-3">
+                            <input type="text" name="nombre" class="form-control form-floating mb-3" placeholder="nombre" >
+                            <label for="text">Nombre </label>
+                            @error('nombre')
+                            <small class="text-danger ">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        
+        
+                        <div class="form-floating mb-3">
+                            <input type="decimal" name="ingredientes"  class="form-control form-floating mb-3" placeholder="ingredientes"  >
+                            <label for="text">Ingredientes </label>
+                            @error('ingredientes')
+                            <small class="text-danger ">{{ $message }}</small>
+                            @enderror
+                        </div>
+        
+                        <div class="form-floating mb-3">
+                            <input type="text" name="indicaciones"  class="form-control form-floating mb-3" placeholder="indicaciones" >
+                            <label for="text">Indicaciones </label>
+                            @error('indicaciones')
+                                <small class="text-danger ">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" name="precio" class="form-control form-floating mb-3" placeholder="precio" >
+                            <label for="text">Precio </label>
+                            @error('precio')
+                                <small class="text-danger ">{{ $message }}</small>
+                            @enderror
+                        </div>
+        
+                        <center>
+                            <button type="" class="btn btn-outline-warning btn-lg"><h4>Guardar</h4></button><br>
+                            <a href="{{route('farmacos.index')}}" class="btn btn-outline-primary mt-3">Regresar</a>
+                        </center>
+        
+                    </article>
+                    <article class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+                    </article>
+                
+                </section>
+        
+                                            
+            </form>
+
+        </div>
+        
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
---
+++ farmacia/resources/views/dashboard/farmacos/index.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+    <title>Farmacos</title>
+</head>
+<body>
+
+    
+    <header>@include('dashboard.partials.nav-header-admin')</header>
+
+    <!-- Si hay cualquier tipo de error se generara algo -->
+  
+    <!-- Siempre colocar el endif -->
+
+    <main>
+        <br><br>
+        <div class="container">
+            <div class="card">
+                
+                <div class="card-header">
+                    <a href="{{ route('farmacos.create') }}" class="btn btn-primary">Crear</a>
+                    <table class="table table-striped">
+                <Thead>
+                    <body>
+                        <tr>
+                            <td>
+                                Id
+                            </td>
+                            <td>
+                                Codigo
+                            </td>
+                        
+                            <td>
+                                Nombre
+                            </td>
+                        
+                            <td>
+                                Ingredientes
+                            </td>
+                        
+                            <td>
+                                Indicaciones
+                            </td>
+                        
+                            <td>
+                                Precio
+                            </td>
+
+                            <td>
+                                Creado
+                            </td>
+                            <td>
+                                Actualizado
+                            </td>
+                            <td>
+                                Acciones
+                            </td>
+                        </tr>
+                        
+                    </body>
+
+                    {{-- recorrer arreglo --}}
+                    {{-- la variable posts que se manda aqui se va a llamar post --}}
+                    @foreach ($farmacos as $farmaco)
+                        <tr>
+                            <td>
+                                {{$farmaco->id}}
+                            </td>
+                        
+                            <td>
+                                {{$farmaco->codigo}}
+                            </td>
+                        
+                            <td>
+                                {{$farmaco->nombre}}
+                            </td>
+                            <td>
+                                {{$farmaco->ingredientes}}
+                            </td>
+                        
+                            <td>
+                                {{$farmaco->indicaciones}}
+                            </td>
+                        
+                            <td>
+                                {{$farmaco->precio}}
+                            </td>
+                        
+                            <td>
+                                {{$farmaco->created_at->format('d-m-Y')}}
+                            </td>
+                        
+                            <td>
+                                {{$farmaco->updated_at-> format('d-m-Y')}}
+                            </td>
+
+                            <td>
+                                <a href="{{route('farmacos.show',$farmaco->id)}}" class="btn btn-outline-info">Ver</a>
+                                <a href="{{route('farmacos.edit',$farmaco->id)}}" class="btn btn-outline-info">Actualizar</a>
+                                                        
+                                <form method="POST" action="{{route('farmacos.destroy',$farmaco->id)}}">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-outline-danger ms-5" type="submit">Borrar</button>
+                                </form >
+
+
+                                
+                            </td>
+                        </tr>
+                    @endforeach
+                        
+
+                </Thead>
+                {{$farmacos->links()}}
+            </table>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
